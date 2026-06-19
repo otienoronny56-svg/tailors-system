6df8cebfe47d285e80552917d00681e10f22d288
Ronny <otienoronny56@gmail.com>
2026-06-19 12:05:01 +0300

    Configure Express to hide .html extensions

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname), {
     etag: true,
     lastModified: true,
+    extensions: ['html']
 }));
 
 // Fallback: serve index.html for root
